c671ec498e66a613303600ae26e17b9853ab253e
PErcyObayuwanA <py4christ@yahoo.com>
2021-11-13 16:43:31 +0000

    added even.js

---
+++ loopy-lighthouse.js
@@ -0,0 +1,13 @@
+
+let start = 100;
+while (start <= 200) {
+  if (start % 12 === 0) {
+    console.log("LoopyLighthouse")
+  }else if (start % 3 === 0) {
+    console.log("Loopy")
+  }else if (start % 4 === 0) {
+    console.log("Lighthouse")
+  }
+  start++;
+
+}
